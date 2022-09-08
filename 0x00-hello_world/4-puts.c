4e0cfffa0c6b3f63444d625de5688485caf86e35
allenjosh266 <allenjosh266@gmail.com>
2022-09-08 15:31:16 -0700

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include	<stdio.h>
+/**
+ * main	-	Entry point
+ * Return Always 0 (Succesful)
+ * */
+int	main	(void)
+{
+ 	puts("\"Programming is like building a multilingual puzzle")i;
+	return	(0);
+}
