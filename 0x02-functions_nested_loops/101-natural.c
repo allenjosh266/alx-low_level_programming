6483ff37716bda498899f4519c7fa472cc825d94
allenjosh266 <allenjosh266@gmail.com>
2022-09-13 18:08:35 -0700

    tasks

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,40 +1,20 @@
+Task 13
 #include <stdio.h>
-
 /**
- * main - Program start point
- * Description: prints the first 50 Fibonacci numbers, starting with 1 and 2
- * Return: 0 on success
+ * main - prints natural numbers below 1024 that are
+ * multiples of 3 or 5
+ *
+ * Return: Always 0.
  */
 int main(void)
 {
-	/* starting numbers */
-	long int x = 1;
-	long int y = 2;
+	int a, b;
 
-	/* counts how many numbers have been computed*/
-	int counter = 2;
-
-	/* temporary variable to hold x */
-	long int tmp;
-
-	printf("%li, %li, ", x, y);
-
-	while (counter < 50)
-	{
-		printf("%li", x + y);
-
-		if (counter == 49)
-			break;
-
-		printf(", ");
-		tmp = x;
-		x = y;
-		y = x + tmp;
-
-		counter++;
-	}
-	putchar('\n');
-
+	for (a = 1; a < 1024; a++)
+{
+	if ((a % 3) == 0 || (a % 5) == 0)
+	b += a;
+}
+	printf("%d\n", b);
 	return (0);
 }
-
