bb0e5594d13af89ba6b06a09f37ab83774e8a206
allenjosh266 <allenjosh266@gmail.com>
2022-09-13 17:47:08 -0700

    task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -30,3 +30,16 @@ void print_times_table(int n)
 	_putchar ((op / 10) + 48);
 	_putchar ((op % 10) + 48);
 }
+	else
+{
+	_putchar (((op / 100) % 10) + 48);
+	_putchar (((op / 10) % 10) + 48);
+	_putchar ((op % 10) + 48);
+}
+}
+{
+	_putchar ('\n');
+}
+}
+}
+}
